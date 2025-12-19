93659afae26ba3c7aadfb133067e37c8bdb280a4
bala-amypo <bala@amypo.in>
2025-12-19 15:39:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/ClaimRepository.java
+++ src/main/java/com/example/demo/repository/ClaimRepository.java
@@ -5,11 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ClaimRepository extends JpaRepository<Claim, Long> {
-
-    List<Claim> findByPolicyId(Long policyId);
-}
-
 
 public interface ClaimRepository extends JpaRepository<Claim, Long> {
     List<Claim> findByPolicy_Id(Long policyId);
