bdd6590be96b06c2e65ee255e3c867ab5edf4389
bala-amypo <bala@amypo.in>
2025-12-26 16:18:08 +0000

    User commit

---
+++ src/test/java/com/example/demo/DemoApplicationContextTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+public class DemoApplicationContextTest {
+
+    @Test
+    public void contextLoads() {
+        // If Spring Boot starts, this test passes
+    }
+}
