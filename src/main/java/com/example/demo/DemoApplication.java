788a2324667d86aede1da320a82a45e35c684ec8
bala-amypo <bala@amypo.in>
2025-12-26 16:03:05 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,14 @@
+
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
 
-@SpringBootApplication
-public class DemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        // Ensures Spring context loads successfully
     }
 }
