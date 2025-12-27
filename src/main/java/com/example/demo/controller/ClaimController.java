450bf27c222321b67f6c8a07beca5dd503fb855d
bala-amypo <bala@amypo.in>
2025-12-27 07:44:19 +0000

    User commit

--- src/main/java/com/example/demo/controller/ClaimController.java
+++ src/main/java/com/example/demo/controller/ClaimController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.ClaimDto;
-import com.example.demo.entity.Claim;
+import com.example.demo.model.Claim;
 import com.example.demo.service.ClaimService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
